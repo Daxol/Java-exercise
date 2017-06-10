8b681de62c2615dfdc7622e46664cebe513236a1
Daxol <daniel.pabjan@edu.uekat.pl>
2017-06-10 14:54:25 +0200

    10

---
+++ src/pliki/pliki/zadanie10/MyClass.java
@@ -0,0 +1,15 @@
+package pliki.pliki.zadanie10;
+
+/**
+ * Created by daniel on 10.06.17.
+ */
+/*
+Napisać funkcję przepisz(String nazwaWe, String nazwaWy, string fraza) która przepisuje dane z pliku tekstowego według zasad:
+w parzystych wierszach pierwsze wystąpienie frazy (podanej jako trzeci parametr) zamienia na frazę pisaną dużymi literami
+np fraza na FRAZA
+w nieparzystych wierszach wszystkie wielkie litery zamienia na znak 'm'
+na końcu pliku należy dopisać wartość odpowiadającą średniej długości lini. jeśli plk wejsciowy nie istnieje należy w pliku
+wyjsciowym wpisac infomracje brak pliku wejsciowego
+ */
+public class MyClass {
+}
