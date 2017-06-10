5852010de8037d781c1ae6eaecbb92a2fcf738b2
Daxol <daniel.pabjan@edu.uekat.pl>
2017-06-10 15:30:38 +0200

    10

--- src/pliki/pliki/zadanie10/MyClass.java
+++ src/pliki/pliki/zadanie10/MyClass.java
@@ -1,5 +1,7 @@
 package pliki.pliki.zadanie10;
 
+import java.io.*;
+
 /**
  * Created by daniel on 10.06.17.
  */
@@ -12,4 +14,51 @@ na końcu pliku należy dopisać wartość odpowiadającą średniej długości 
 wyjsciowym wpisac infomracje brak pliku wejsciowego
  */
 public class MyClass {
+
+    public static void main(String[] args) throws IOException {
+        try {
+
+            przepisz("zadanie10.txt", "zadanie10_1.txt", "dom");
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+    }
+
+    private static void przepisz(String nazwaWe, String nazwaWy, String fraza) throws IOException {
+        boolean fileExist = new File(nazwaWe).isFile();
+        String line;
+        char[] lineChars;
+
+        LineNumberReader file = null;
+        FileWriter writer = new FileWriter(nazwaWy);
+
+        if (fileExist) {
+            file = new LineNumberReader(new FileReader(nazwaWe));
+
+            try {
+
+                while ((line = file.readLine()) != null) {
+                    lineChars = line.toCharArray();
+                    if (file.getLineNumber() % 2 == 0) {
+                        line = line.replaceFirst(fraza, fraza.toUpperCase());
+                        writer.write(line);
+
+                    } else {
+                        for (int i = 0; i <= lineChars.length - 1; i++) {
+                            if (Character.isUpperCase(lineChars[i])) {
+                                lineChars[i] = 'm';
+                            }
+                        }
+                        writer.write(String.valueOf(lineChars));
+                    }
+                    writer.write("\n");
+                }
+            } catch (IOException exception) {
+                file.close();
+            }
+        } else {
+            writer.write("brak pliku wejsciowego");
+        }
+        writer.close();
+    }
 }
